03748f6b1f9e8ac55f74e2ff26d18ec05766df8e
Nitun_shivay <nitinshivbaba499@gmail.com>
2021-07-04 12:40:27 -0700

    Added questions

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define fr(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+class Parent{
+    public:
+    Parent(){
+        cout<<"Constructor of parent"<<endl;
+    }
+};
+class Child :public Parent{
+    public:
+    Child(){
+        cout<<"Constructor of child class "<<endl;
+    }
+};
+
+int main(){
+    Child obj;
+    
+    return 0;
+}
